ea9cfe5919a4c242a17d487d2175e06aab97a75a
Suomynona589 <suomynona589@gmail.com>
2026-01-12 16:14:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { 
+  getAuth, 
+  browserLocalPersistence,
+  browserSessionPersistence,
+  setPersistence,
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCT8uB4fTOPFsTuVQfUkrI55247iKAxiLQ",
+  authDomain: "sign-in-thing-7f106.firebaseapp.com",
+  projectId: "sign-in-thing-7f106",
+  storageBucket: "sign-in-thing-7f106.firebasestorage.app",
+  messagingSenderId: "215017768496",
+  appId: "1:215017768496:web:e71afda540582184bb8ff5"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signupBtn = document.getElementById("signupBtn");
+const loginBtn = document.getElementById("loginBtn");
+const message = document.getElementById("message");
+
+signupBtn.onclick = () => {
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => message.textContent = err.message);
+};
+
+loginBtn.onclick = () => {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+  const remember = document.getElementById("rememberMe").checked;
+
+  const persistence = remember ? browserLocalPersistence : browserSessionPersistence;
+
+  setPersistence(auth, persistence)
+    .then(() => {
+      return signInWithEmailAndPassword(auth, email, password);
+    })
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => message.textContent = err.message);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.body;
 
