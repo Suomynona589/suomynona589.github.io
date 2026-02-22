980a38b71a1c6efa740f586343626faf91e2295c
Suomynona589 <suomynona589@gmail.com>
2026-02-22 14:02:04 -0700

    Update script.js

--- adblock-tester/extreme/script.js
+++ adblock-tester/extreme/script.js
@@ -41,46 +41,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (chosen === "static") {
         container.innerHTML = `
-        <div class="test-box extreme-box">
-            <h2>Static Image Ad</h2>
-            <img src="/images/ads/static-image.png" class="extreme-ad">
-        </div>`;
+        <div class="test-box" data-test="1">
+        <h2>Static Image Ad</h2>
+        <p class="note">Should be blocked</p>
+        <img src="/images/ads/static-image.png" class="ad ad-banner static" alt="Static Ad Image">
+    </div>`;
     }
 
     if (chosen === "gif") {
         container.innerHTML = `
-        <div class="test-box extreme-box">
-            <h2>GIF Image Ad</h2>
-            <img src="https://c.tenor.com/bxe8Qsx3UusAAAAC/tenor.gif" class="extreme-ad">
-        </div>`;
+        <div class="test-box" data-test="2">
+        <h2>GIF Image Ad</h2>
+        <p class="note">Should be blocked</p>
+        <img src="https://c.tenor.com/bxe8Qsx3UusAAAAC/tenor.gif" class="ad ad-banner" alt="GIF Ad">
+    </div>`;
     }
 
     if (chosen === "sponsor") {
         container.innerHTML = `
-        <div class="test-box extreme-box">
-            <h2>Sponsored Content Block</h2>
-            <div class="sponsored-block extreme-ad">
-                <span class="sponsored-label">Sponsored</span>
-                <p>This is a fake sponsored article preview with ad-like styling.</p>
-                <img src="/images/ads/sponsor-logo.png" class="sponsored">
-            </div>
-        </div>`;
+        <div class="test-box" data-test="3">
+        <h2>Sponsored Content Block</h2>
+        <p class="note">Often blocked by cosmetic filters</p>
+        <div class="sponsored-block">
+            <span class="sponsored-label">Sponsored</span>
+            <p>This is a fake sponsored article preview with ad-like styling.</p>
+            <img src="/images/ads/sponsor-logo.png" class="sponsored" alt="Sponsor Logo">
+        </div>
+    </div>`;
     }
 
     if (chosen === "iframe") {
         container.innerHTML = `
-        <div class="test-box extreme-box">
-            <h2>Iframe Ad</h2>
-            <iframe src="https://example.com" class="extreme-ad" width="300" height="100"></iframe>
-        </div>`;
+        <div class="test-box" data-test="4">
+        <h2>Iframe Ad</h2>
+        <p class="note">Should be blocked</p>
+        <iframe
+            src="https://example.com"
+            class="ad iframe-ad"
+            width="300"
+            height="100"
+            title="Fake Iframe Ad">
+        </iframe>
+    </div>`;
     }
 
     if (chosen === "flash") {
         container.innerHTML = `
-        <div class="test-box extreme-box">
-            <h2>Flash Banner</h2>
-            <object data="/images/ads/flash-banner.swf" class="extreme-ad" width="300" height="250"></object>
-        </div>`;
+        <div class="test-box" data-test="5">
+        <h2>Flash Banner</h2>
+        <p class="note">Should be blocked</p>
+        <object
+            data="/images/ads/flash-banner.swf"
+            class="ad flash swf"
+            width="300"
+            height="250">
+        </object>
+        <p class="note">Browsers will not play this, but it is useful for detection tests.</p>
+    </div>`;
     }
 
     const ad = document.querySelector(".extreme-ad");
