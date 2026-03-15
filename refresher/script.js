2046a4b3cc8c4a85113d4673e066527cb9c01ff2
Suomynona589 <suomynona589@gmail.com>
2026-03-15 12:26:54 -0600

    Create script.js

---
+++ refresher/script.js
@@ -0,0 +1,6 @@
+let count = sessionStorage.getItem("refreshCount")
+if (!count) count = 0
+count++
+sessionStorage.setItem("refreshCount", count)
+document.getElementById("score").textContent = count
+document.getElementById("refreshBtn").onclick = () => location.reload()
