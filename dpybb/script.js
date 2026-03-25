b361d1e580712c2c9b3a4fa4567626005978fde5
Suomynona589 <suomynona589@gmail.com>
2026-03-25 16:00:03 -0600

    Update script.js

--- dpybb/script.js
+++ dpybb/script.js
@@ -1,11 +1,16 @@
 if (localStorage.getItem("connected-to-bot") === "true") {
-    location.href = "/dpybb/builder";
+    location.href = "builder";
 }
 
-document.getElementById("submit").onclick = function () {
-    let t = document.getElementById("token").value;
-    if (t.length < 10) return;
-    localStorage.setItem("bot-token", t);
-    localStorage.setItem("connected-to-bot", "true");
-    location.href = "/dpybb/builder";
+window.onload = function () {
+    const btn = document.getElementById("submit");
+    const token = document.getElementById("token");
+
+    btn.onclick = function () {
+        let t = token.value;
+        if (t.length < 10) return;
+        localStorage.setItem("bot-token", t);
+        localStorage.setItem("connected-to-bot", "true");
+        location.href = "builder";
+    };
 };
